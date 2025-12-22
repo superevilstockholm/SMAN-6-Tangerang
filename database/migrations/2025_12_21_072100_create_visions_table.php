5b67f368d556ab3ec8142eb1505ea939db67631f
superevilstockholm <balilombok11@gmail.com>
2025-12-22 17:19:43 +0700

    Updated change field content data type from string VARCHAR(255) to text TEXT

--- database/migrations/2025_12_21_072100_create_visions_table.php
+++ database/migrations/2025_12_21_072100_create_visions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('visions', function (Blueprint $table) {
             $table->id();
-            $table->string('content');
+            $table->text('content');
             $table->boolean('is_active')->default(false);
             $table->timestamps();
         });

b86677d4da149f0e6f560df6b45f00f03a6d2680
superevilstockholm <balilombok11@gmail.com>
2026-01-05 19:56:27 +0700

    Created index method in activity log controller

--- app/Http/Controllers/Settings/ActivityLogController.php
+++ app/Http/Controllers/Settings/ActivityLogController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Settings;
 
+use Throwable;
+use Carbon\Carbon;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 
 // Models
 use App\Models\Settings\ActivityLog;
@@ -13,9 +17,52 @@ class ActivityLogController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index(Request $request): View | RedirectResponse
     {
-        //
+        try {
+            $limit = $request->query('limit', 10);
+            $query = ActivityLog::query()->orderBy('created_at', 'desc')->with('user');
+            // Search
+            $allowed_types = ['method', 'path', 'user_id', 'user_name', 'ip_address', 'user_agent', 'status_code', 'date'];
+            $type = $request->query('type');
+            if ($type && in_array($type, $allowed_types)) {
+                if ($type === 'date') {
+                    $start_date = $request->query('start_date');
+                    $end_date = $request->query('end_date');
+                    if ($start_date) {
+                        $start_date = Carbon::parse($start_date)->startOfDay();
+                        $query->where('created_at', '>=', $start_date);
+                    }
+                    if ($end_date) {
+                        $end_date = Carbon::parse($end_date)->endOfDay();
+                        $query->where('created_at', '<=', $end_date);
+                    }
+                } else {
+                    $search = $request->query('search');
+                    if ($search) {
+                        if ($type === 'user_name') {
+                            $query->whereHas('user', function ($q) use ($search) {
+                                $q->where('name', 'like', '%' . $search . '%');
+                            });
+                        } else {
+                            $query->where($type, 'like', '%' . $search . '%');
+                        }
+                    }
+                }
+            }
+            $activity_logs = $limit === 'all'
+                ? $query->get()
+                : $query->paginate((int) $limit)
+                    ->appends($request->except('page'));
+            return view('pages.dashboard.admin.settings.activity-log.index', [
+                'meta' => [
+                    'sidebarItems' => adminSidebarItems(),
+                ],
+                'activity_logs' => $activity_logs,
+            ]);
+        } catch (Throwable $e) {
+            return redirect()->route('dashboard.admin.settings.activity-logs.index')->withErrors($e->getMessage());
+        }
     }
 
     /**
